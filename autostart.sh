014d99b2dbc487642f0fc86ca84368b838a3fd63
thaoeu <154942630@qq.com>
2020-02-21 15:18:19 +0800

    add sleep time

--- autostart.sh
+++ autostart.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
 
-kdeconnect-cli
 # test zone
 xrandr --output DP-0 --off
-picom -o 0.95 -i 0.88 --detect-rounded-corners --vsync --blur-background-fixed -f -D 5 -c -b
 bluetoothctl power on
 bluetoothctl &
-# nm-applet &
-/bin/bash ~/Script/dwm-status.sh &
+picom -o 0.95 -i 0.88 --detect-rounded-corners --vsync --blur-background-fixed -f -D 5 -c -b
 /bin/bash ~/Script/wp-autochange.sh &
+/bin/bash ~/Script/dwm-status.sh &
 
-# keynav&
+sleep 10
+kdeconnect-cli
 konsole -e nvim ~/Templates/hgd.md &
 konsole -e nvim
 # konsole -e nvim ~/Overt/config/vim/init.vim
-#nm-applet &
 #xfce4-power-manager &
 #xfce4-volumed-pulse &
 #~/Script/autostart_wait.sh &
