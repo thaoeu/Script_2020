19252f11183545333041784e2cfc4372591d4e61
thaoeu <154942630@qq.com>
2020-03-16 20:34:46 +0800

    Fine-tuned appearance

--- autostart.sh
+++ autostart.sh
@@ -5,14 +5,13 @@ xrandr --output DP-0 --off
 bluetoothctl power on
 bluetoothctl &
 picom -o 0.95 -i 0.88 --detect-rounded-corners --vsync --blur-background-fixed -f -D 5 -c -b
-/bin/bash ~/Script/wp-autochange.sh &
 /bin/bash ~/Script/dwm-status.sh &
+/bin/bash ~/Script/wp-autochange.sh &
+/bin/bash ~/Script/kde-start.sh &
 
-kdeconnect-cli
-krunner &
-konsole -e nvim ~/Templates/hgd.md &
+#kdeconnect-cli
+#konsole -e nvim ~/Templates/hgd.md &
 #konsole -e nvim ~/Templates/qmk_firmware/keyboards/bggpad/keymaps/default/keymap.c
 # konsole -e nvim ~/Overt/config/vim/init.vim
 #xfce4-power-manager &
 #xfce4-volumed-pulse &
-#~/Script/autostart_wait.sh &
---
+++ kde-start.sh
@@ -0,0 +1,5 @@
+kdeconnect-cli
+krunner
+konsole -e nvim ~/Templates/hgd.md
+
+konsole -e nvim ~/Overt/config/vim/init.vim
