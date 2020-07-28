f9effb12d5a1ae19f4a320007b98e2419760a02f
thaoeu <154942630@qq.com>
2020-07-28 19:47:39 +0800

    整了个托盘装作 Dock 栏

--- trayer.sh
+++ trayer.sh
@@ -1,6 +1,14 @@
-result=$(ps ax|grep -v grep|grep trayer)
-if [ "$result" == "1" ]; then
-	eval "trayer --transparent true --expand false --align left --width 20 --SetDockType false --tint 0x66ccff &"
-else
-	eval "killall trayer"
+#!/bin/bash
+# result=$(ps ax|grep -v grep|grep trayer)
+# if [ "$result" == "1" ]; then
+# 	eval "trayer --transparent true --expand false --align left --width 20 --SetDockType false --tint 0x66ccff &"
+# 
+# else
+# 	eval "killall trayer"
+# fi
+killall trayer
+if [ $? = 0 ];then
+	echo "was kill"
+else [ $? != 0 ]
+	trayer --transparent flase --expand false --align center --width 60 --height 60 --SetDockType false --tint 0x00 &
 fi
