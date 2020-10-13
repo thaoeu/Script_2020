be2f2ce39545a14258fee34b9a1e95c6dc627b89
thaoeu <154942630@qq.com>
2020-10-13 13:55:10 +0800

    修复 Screenkey

--- Software/sck-tog.sh
+++ Software/sck-tog.sh
@@ -3,7 +3,7 @@
 result=$(ps ax|grep -v grep|grep screenkey)
 if [ "$result" == "" ]; then
 	#  eval "screenkey --bg-color white --font-color black &"
-	eval "screenkey"
+	eval "screenkey &"
 else
 	eval "killall screenkey"
 fi
