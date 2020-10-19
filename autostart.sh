994e2f800587ecfbce57416da6802fe1cf0a7969
thaoeu <154942630@qq.com>
2020-10-19 17:41:01 +0800

    just 60 终于能用了

--- autostart.sh
+++ autostart.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
-xrandr --output DP-0 --mode 1920x1080 --pos 0x0 --rotate normal --output DP-1 --mode 1920x1080 --pos 1920x0 --rotate normal
+xrandr --output HDMI-0 --off --output DP-0 --mode 1920x1080 --pos 0x0 --rotate normal --output DP-1 --mode 1920x1080 --pos 1920x0 --rotate normal --output DP-2 --off --output DP-3 --off --output DP-4 --off
+
 #xrandr --auto --output DP-0 --pos 0x00 --mode 1920x1080 --output HDMI-0 --mode 1920x1080 --primary --left-of DP-0
 #xrandr --output HDMI-0 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP-0 --mode 1920x1080 --pos 1920x0 --rotate left
 #xrandr --output DP-3 --primary --mode 1920x1080 --rate 60 --pos 0x0 --output DP-0 --mode 1920x1080 --pos 1920x10
 ~/Script/Video/wp-change.sh &
-# bluetoothctl power on &
-# bluetoothctl &
+bluetoothctl power on &
+bluetoothctl &
 # nm-applet &
 # picom -o 0.95 -i 0.88 --detect-rounded-corners --vsync --blur-background-fixed -f -D 5 -c -b
 
