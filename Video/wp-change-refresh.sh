58caea01804226ffe7a9bcb9143f74daebb83916
thaoeu <thaoeu@thaoeu.site>
2021-02-06 21:09:17 +0800

    无关紧要的目录问题

--- Video/wp-change-refresh.sh
+++ Video/wp-change-refresh.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
-	feh --recursive --randomize --bg-fill ~/Picture
+feh --recursive --randomize --bg-fill ~/Picture/PC_16x9
+#	feh --recursive --randomize --bg-fill ~/Picture/S_9x18
 #	feh --recursive --bg-fill ~/Pictures/Puts/23Ven-Mountain.jpg
